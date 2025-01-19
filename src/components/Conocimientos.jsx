f512a66ec2a2f7f80d723f8ef6e69cdd63137737
Calu <lucas_britez@hotmail.com>
2025-01-18 22:28:48 -0300

    casi terminamos conocimientos

--- src/components/Conocimientos.jsx
+++ src/components/Conocimientos.jsx
@@ -10,49 +10,113 @@ export const Conocimientos = () => {
   return (
     <div id='box-main-conocimientos'>
 
-      <div id='boxMain-front-developer'>
+      <div id='boxMain-conocimientos-volver'>
+        <button className='boton-beige'>Volver</button>
 
-        <div id='boxMain-front-developer-img'>
+      </div>
+
+      <div id='boxMain-developer'>
+
+        <div id='boxMain-developer-img'>
           <img src={imgDeveloper} alt="" />
         </div>
 
-        <div id='boxMain-front-developer-skills'>
-          <div id='boxMain-front-developer-skills-textOne'>
+        <div id='boxMain-developer-skills'>
+
+          <div id='boxMain-developer-skills-textOne'>
             <p>Los lenguajes y las tecnologías que estoy utilizando actualmente en mis proyectos son:</p>
           </div>
-          <div id='boxMain-front-developer-skills-frontDev'>
-            <div id='boxMain-front-developer-skills-frontDev-text'>
+
+          <div id='boxMain-developer-skills-frontDev'>
+
+            <div id='boxMain-developer-skills-frontDev-text'>
               <p>Frontend:</p>
             </div>
-            <div id='boxMain-front-developer-skills-frontDev-icons'>
+            <div id='boxMain-developer-skills-frontDev-icons'>
               <Skill technology="React" />
               <Skill technology="JS" />
               <Skill technology="Bootstrap" />
               <Skill technology="GoogleMaps" />
+              <Skill technology="HTML" />
+              <Skill technology="CSS" />
+            </div>
+          </div>
+
+          <div id='boxMain-developer-skills-backDataBaseDev'>
+            
+            <div id='boxMain-developer-skills-backDev'>
+              <div id='boxMain-developer-skills-backDev-text'>
+                <p>Backend:</p>
+              </div>
+              <div id='boxMain-developer-skills-backDev-icons'>
+                <Skill technology="NodeJS" />
+                <Skill technology="Express" />
+              </div>
+            </div>
+            
+            <div id='boxMain-developer-skills-dataBaseDev'>
+              <div id='boxMain-developer-skills-dataBaseDev-text'>
+                <p>DataBase:</p>
+              </div>
+              <div id='boxMain-developer-skills-dataBaseDev-icons'>
+                <Skill technology="MySQL" />
+                <Skill technology="MongoDB" />
+              </div>
+
+            </div>
+
+          </div>
+
+          <div id='boxMain-developer-skills-verControlDesignDev'>
+
+            <div id='boxMain-developer-skills-verControlDev'>
+              <div id='boxMain-developer-skills-verControlDev-text'>
+                <p>Version Control:</p>
+              </div>
+              <div id='boxMain-developer-skills-verControlDev-icons'>
+                <Skill technology="Git" />
+                <Skill technology="GitHub" />
+              </div>
+            </div>
+
+            <div id='boxMain-developer-skills-designDev'>
+              <div id='boxMain-developer-skills-designDev-text'>
+                <p>Design:</p>
+              </div>
+              <div id='boxMain-developer-skills-designDev-icons'>
+                <Skill technology="Illustrator" />
+                <Skill technology="Photoshop" />
+              </div>
+            </div>
+
+          </div>
+
+          <div id='boxMain-developer-skills-textTwo'>
+            <p>Además, tengo conocimientos en:</p>
+          </div>
+
+          <div id='boxMain-developer-skills-extraDev'>
+            <div id='boxMain-developer-skills-extraDev-firstBox'>
+              <Skill technology="ReactNative" />
+              <Skill technology="PHP" />
+              <Skill technology="Python" />
+              <Skill technology="Typescript" />
+            </div>
+            <div id='boxMain-developer-skills-extraDev-secondBox'>
+              <Skill technology="Trello" />
+              <Skill technology="Jira" />
+            </div>
+            <div id='boxMain-developer-skills-extraDev-thirdBox'>
+              <Skill technology="Docker" />
+              <Skill technology="Socket.io" />
+              <Skill technology="Tensorflow" />
+            </div>
+            <div id='boxMain-developer-skills-extraDev-fourthBox'>
+              <Skill technology="AutoCAD" />
             </div>
           </div>
 
         </div>
-
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <div id='boxMain-back-developer'>
-
-      </div>
-
-      <div id='boxMain-extra-developer'>
 
       </div>
 
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,6 +4,30 @@ import { FaReact } from "react-icons/fa";
 import { FaJsSquare } from "react-icons/fa";
 import { FaBootstrap } from "react-icons/fa";
 import { SiGooglemaps } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { TbBrandMysql } from "react-icons/tb";
+import { SiMongodb } from "react-icons/si";
+import { SiSequelize } from "react-icons/si";
+import { SiMongoose } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiAdobephotoshop } from "react-icons/si";
+import { TbBrandReactNative } from "react-icons/tb";
+import { SiPhp } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
+import { IoLogoDocker } from "react-icons/io5";
+import { SiSocketdotio } from "react-icons/si";
+import { SiTensorflow } from "react-icons/si";
+import { FaTrello } from "react-icons/fa";
+import { SiJira } from "react-icons/si";
+import { SiAutocad } from "react-icons/si";
+
+
 
 
 export const Skill = ({ technology }) => {
@@ -28,6 +52,101 @@ export const Skill = ({ technology }) => {
       icon = <SiGooglemaps />;
       text = "GoogleMaps";
       break;
+    case "HTML":
+      icon = <FaHtml5 />;
+      text = "HTML";
+      break;
+    case "CSS":
+      icon = <FaCss3Alt />;
+      text = "CSS";
+      break;
+    case "NodeJS":
+      icon = <FaNodeJs />;
+      text = "NodeJS";
+      break;
+    case "Express":
+      icon = <SiExpress />;
+      text = "Express";
+      break;
+    case "MySQL":
+      icon = <TbBrandMysql />;
+      text = "MySQL";
+      break;
+    case "MongoDB":
+      icon = <SiMongodb />;
+      text = "MongoDB";
+      break;
+    case "Sequelize":
+      icon = <SiSequelize />;
+      text = "Sequelize";
+      break;
+    case "Mongoose":
+      icon = <SiMongoose />;
+      text = "Mongoose";
+      break;
+    case "Git":
+      icon = <FaGitAlt />;
+      text = "Git";
+      break;
+    case "GitHub":
+      icon = <FiGithub />;
+      text = "Github";
+      break;
+    case "Illustrator":
+      icon = <SiAdobeillustrator />;
+      text = "Illustrator";
+      break;
+    case "Photoshop":
+      icon = <SiAdobephotoshop />;
+      text = "Photoshop";
+      break;
+    case "ReactNative":
+      icon = <TbBrandReactNative />;
+      text = "React Native";
+      break;
+    case "PHP":
+      icon = <SiPhp />;
+      text = "PHP";
+      break;
+    case "Python":
+      icon = <FaPython />;
+      text = "Python";
+      break;
+    case "Typescript":
+      icon = <TbBrandTypescript />;
+      text = "Typescript";
+      break;
+    case "Docker":
+      icon = <IoLogoDocker />;
+      text = "Docker";
+      break;
+    case "Socket.io":
+      icon = <SiSocketdotio />;
+      text = "Socket.io";
+      break;
+    case "Tensorflow":
+      icon = <SiTensorflow />;
+      text = "Tensorflow";
+      break;
+    case "Trello":
+      icon = <FaTrello />;
+      text = "Trello";
+      break;
+    case "Jira":
+      icon = <SiJira />;
+      text = "Jira";
+      break;
+    case "AutoCAD":
+      icon = <SiAutocad />;
+      text = "AutoCAD";
+      break
+    
+
+
+
+
+
+
     default:
       text = "Unknown";
   }

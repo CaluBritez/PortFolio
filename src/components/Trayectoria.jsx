48b5adbe063994c4ef40196f39b3606eecc15b92
Calu <lucas_britez@hotmail.com>
2025-01-26 18:25:35 -0300

    caja años terminada

--- src/components/Trayectoria.jsx
+++ src/components/Trayectoria.jsx
@@ -1,6 +1,43 @@
+import './css/Trayectoria.css'
+
+import { useDispatch } from 'react-redux';
+
+import { home } from '../store/ui/uiSlice';
+
+import 'animate.css';
+
 
 export const Trayectoria = () => {
+
+  const dispatch = useDispatch();
+
   return (
-    <div>Trayectoria</div>
+    <div id='box-main-trayectoria' className='animate__animated animate__zoomIn'>
+
+      <div id='box-main-trayectoria-caja-cerrar'>
+        <button className='boton-lila-chico' onClick={() => dispatch(home())}>Volver</button>
+      </div>
+
+      <div id='box-main-trayectoria-years-content'>
+
+        <div id='box-main-trayectoria-years'>
+          <div id='box-main-trayectoria-years-armario'>
+            <p href="">2024</p>
+            <p href="">2023</p>
+            <p href="">2022</p>
+            <p href="">2021</p>
+            <p href="">2020 - 2021</p>
+          </div>
+        </div>
+
+        <div id='box-main-trayectoria-content'>
+        </div>
+      </div>
+
+
+      
+
+
+    </div>
   )
 }
